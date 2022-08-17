f3cfedd9d51bac4b1d5aea4fa397ceedd5f0507b
MarcioPortela <m.spmarcio2@gmail.com>
2022-08-16 22:41:37 -0300

    capturando dados do formulario

--- app/ts/app.ts
+++ app/ts/app.ts
@@ -1,2 +1,5 @@
-const negociacao = new Negociacao(new Date(), 1, 1);
-console.log(negociacao);
+const controller = new NegociacaoController();
+
+document
+    .querySelector('.form')
+    .addEventListener('submit', controller.adiciona.bind(controller));
---
+++ app/ts/controllers/NegociacaoController.ts
@@ -0,0 +1,25 @@
+class NegociacaoController{
+
+    _inputData;
+    _inputQuantidade;
+    _inputValor;
+
+    constructor(){
+        this._inputData = document.querySelector('#data');
+        this._inputQuantidade = document.querySelector('#quantidade');
+        this._inputValor = document.querySelector('#valor');
+    }
+
+    adiciona(event){
+
+        event.preventDefault();
+
+        const negociacao = new Negociacao(
+            this._inputData.value,
+            this._inputQuantidade.value,
+            this._inputValor.value
+        );
+        
+        console.log(negociacao);
+    }
+}
